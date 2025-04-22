aa44992b317f79750ac79365266454d7d4f9ae61
Keu <keuxd2003@gmail.com>
2025-04-22 18:35:43 -0300

    Insets on systemBars removed
    Those insets were moving our BottomNavigationBar up, so its now removed, in the future we might put it back but without the bottom padding

--- app/src/main/java/com/example/sleuthpedia/MainActivity.java
+++ app/src/main/java/com/example/sleuthpedia/MainActivity.java
@@ -7,9 +7,6 @@ import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
 //        String dbPath = getDatabasePath(DatabaseHelper.DB_NAME).toString();
 //        SQLiteDatabase database = SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READWRITE);
