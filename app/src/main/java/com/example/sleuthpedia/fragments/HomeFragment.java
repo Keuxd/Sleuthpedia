1f66dd26385c838077c06680f700b4cc8965f20c
Keu <keuxd2003@gmail.com>
2025-04-22 18:29:01 -0300

    Fragments class/xml added
    Fragments are pieces of a specific screen, in this case we have fragments from Home, Team and Options "screens"

---
+++ app/src/main/java/com/example/sleuthpedia/fragments/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.sleuthpedia.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.sleuthpedia.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
