5677379016e2de3a429a42f81369ba088a631e78
Keu <keuxd2003@gmail.com>
2025-06-17 16:58:06 -0300

    TouchEvent is now being dispatched to close keyboard on outside click

--- app/src/main/java/com/example/sleuthpedia/MainActivity.java
+++ app/src/main/java/com/example/sleuthpedia/MainActivity.java
@@ -1,45 +1,42 @@
 package com.example.sleuthpedia;
 
+import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.sleuthpedia.fragments.HomeFragment;
-import com.example.sleuthpedia.fragments.OptionsFragment;
-import com.example.sleuthpedia.fragments.TeamFragment;
+import com.example.sleuthpedia.fragments.menu.HomeFragment;
+import com.example.sleuthpedia.fragments.menu.OptionsFragment;
+import com.example.sleuthpedia.fragments.menu.TeamFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SQLiteDatabase db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         setupBottomNavigation();
 
-//        logDatabaseVersion();
 
-        db = new DatabaseHelper(this).init();
-//        queryDataTest();
-    }
+        DatabaseHelper.init(this);
 
-    @Override
-    protected void onDestroy() {
-        if(db != null) {
-            db.close();
-        }
-        super.onDestroy();
+        queryDataTest();
     }
 
     private void setupBottomNavigation() {
@@ -55,27 +52,43 @@ public class MainActivity extends AppCompatActivity {
             Fragment fragment = fragmentMap.get(item.getItemId());
             if(fragment == null) return false;
 
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.menuFragmentContainer, fragment).commit();
             return true;
         });
+
+        bottomMenu.setSelectedItemId(R.id.menu_home);
+    }
+
     }
 
     private void queryDataTest() {
-        Cursor cursor = db.rawQuery("SELECT * FROM test", null);
+        Cursor cursor = DatabaseHelper.getInstance().getReadableDatabase().rawQuery("SELECT * FROM digimon", null);
+        StringBuilder sb = new StringBuilder();
 
         if(cursor.moveToFirst()) {
             do {
-                String data = cursor.getString(cursor.getColumnIndexOrThrow("gold"));
-                Log.d("PRINT", data);
+                String data = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+                sb.append(data + " | ");
             } while(cursor.moveToNext());
         }
+        Log.d("PRINT", sb.toString());
         cursor.close();
     }
 
-    private void logDatabaseVersion() {
-        String dbPath = getDatabasePath(DatabaseHelper.DB_NAME).toString();
-        SQLiteDatabase database = SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READWRITE);
-        Log.d("PRINT", "DB VERSION -> " + database.getVersion());
-        database.close();
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 }
